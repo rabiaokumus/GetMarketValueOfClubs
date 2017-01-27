66c3eec32ae6e21fe5ed1c80b8802549b07ad2b1
rabiaokumus <rabiaokumus@gmail.com>
2017-01-27 18:58:15 +0300

    first repository

---
+++ service.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/TR1', function(req, res){
+  url = 'http://www.transfermarkt.com/super-lig/startseite/wettbewerb/TR1';
+
+  request(
+    {
+      uri :url
+    }, function(error, response, html){
+
+      var $ = cheerio.load(html);
+
+     $('#yw1 td').each(function(i, element){
+            var a = $(this).prev();
+            console.log(a.text());
+          });
+
+      res.send(html)
+  })
+})
+
+app.listen('8081')
+console.log('Listening http://127.0.0.1:8081/8081');
+exports = module.exports = app;
