5d453026d277213c73c45b6efedd037535768838
rabiaokumus <rabiaokumus@gmail.com>
2017-02-06 22:42:32 +0300

    edit listener

--- service.js
+++ service.js
@@ -4,7 +4,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/', function(req, res){
+app.get('', function(req, res){
   console.log("router.TR1 entered");
   url = 'http://www.transfermarkt.com/super-lig/startseite/wettbewerb/TR1';
 
@@ -24,8 +24,8 @@ app.get('/', function(req, res){
   })
 })
 
-app.listen(8000, function() {
-  console.log("listening on port 8000");
+app.listen(80 , function() {
+  console.log("listening on port 80");
 });
 
 exports = module.exports = app;
